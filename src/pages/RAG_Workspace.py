127a11df984372113dd3b13bc306fba11f86c32a
patrickab <patrick.a.bitzer@gmail.com>
2025-11-27 22:08:35 +0100

    fetch updates

--- src/pages/RAG_Workspace.py
+++ src/pages/RAG_Workspace.py
@@ -1,13 +1,18 @@
 import os
+from pathlib import Path
 from typing import Optional
 
 import polars as pl
-from rag_database.dataclasses import RAGIngestionPayload
+from rag_database.dataclasses import RAGIngestionPayload, RAGQuery
 from rag_database.rag_config import MODEL_CONFIG, DatabaseKeys
-from rag_database.rag_database import RagDatabase, RAGQuery
+from rag_database.rag_database import RagDatabase
 import streamlit as st
 
-from src.config import DEFAULT_EMBEDDING_MODEL, DIRECTORY_EMBEDDINGS, DIRECTORY_OBSIDIAN_DOCS, DIRECTORY_OBSIDIAN_VAULT, RAG_K_DOCS
+from src.config import (
+    DEFAULT_EMBEDDING_MODEL,
+    DIRECTORY_EMBEDDINGS,
+    DIRECTORY_RAG_INPUT,
+)
 from src.lib.streamlit_helper import nyan_cat_spinner
 
 DATABASE_LABAL_OBSIDIAN = "obsidian"
@@ -18,40 +23,68 @@ def init_rag_workspace() -> None:
         st.session_state.rag_databases = {}
 
 def rag_sidebar() -> None:
+    """RAG Workspace Sidebar for RAG Database selection & initialization."""
+
     with st.sidebar:
+
         st.session_state.selected_embedding_model = st.selectbox(
             "Select Embedding Model",
             options=list(MODEL_CONFIG.keys()),
             index=list(MODEL_CONFIG.keys()).index(DEFAULT_EMBEDDING_MODEL),
         )
 
+        available_database_payloads = os.listdir(DIRECTORY_RAG_INPUT)
+        available_database_embeddings = os.listdir(DIRECTORY_EMBEDDINGS)
+
+        # for all available database embeddings check for all embedding models if they are contained as string & try to trunctate _<embedding_model>.parquet to receive the label # noqa
+        unique_database_labels = set()
+        for embedding_file in available_database_embeddings:
+            for model_name in MODEL_CONFIG:
+                suffix = f"_{model_name}.parquet"
+                if embedding_file.endswith(suffix):
+                    unique_database_labels.add(embedding_file.removesuffix(suffix))
+                    break
+
+        available_database_embeddings = sorted(unique_database_labels)
+        options = set(available_database_payloads + available_database_embeddings)
+
+        st.session_state.selected_rag_database = st.selectbox(
+            "Select RAG Database",
+            options=options,
+            index=0,
+        )
         if st.button("Initialize RAG Database", key="load_rag_db"):
-
             with nyan_cat_spinner():
-                database = load_rag_database(
-                    doc_path=f"{DIRECTORY_OBSIDIAN_VAULT}/{DIRECTORY_OBSIDIAN_DOCS}/",
-                    model=st.session_state.selected_embedding_model,
-                    label=DATABASE_LABAL_OBSIDIAN
-                )
-            st.session_state.rag_databases.setdefault(DATABASE_LABAL_OBSIDIAN, {})[st.session_state.selected_embedding_model] = database # noqa
+                selection = st.session_state.selected_rag_database
+                model = st.session_state.selected_embedding_model
 
-        st.markdown("---")
-        st.markdown("## RAG Databases in Memory")
+                payload_path = Path(f"{DIRECTORY_RAG_INPUT}/{selection}/{selection}_ingestion_payload.parquet")
+                embedding_path = Path(f"{DIRECTORY_EMBEDDINGS}/{selection}/{selection}_embeddings.parquet")
 
-        for label, models_dict in st.session_state.rag_databases.items():
-            with st.expander(f"**Label**:{label}", expanded=False):
-                for model, rag_db in models_dict.items():
-                    with st.expander(f"**Model**:{model}", expanded=False), st.expander("Inspect Database", expanded=False):
-                        st.dataframe(rag_db.vector_db.database)
-                        if st.button("Store Database", key=f"store_rag_db_{label}_{model}"):
-                            parquet_embeddings = f"{DIRECTORY_EMBEDDINGS}/{label}_{model}.parquet"
-                            rag_db.vector_db.database.write_parquet(parquet_embeddings)
-                            st.success(f"Stored RAG Database '{label}' to {parquet_embeddings}")
+                if payload_path.exists() and not embedding_path.exists():
+                    payload = RAGIngestionPayload.from_parquet(payload_path)
+                    rag_db = generate_rag_database(selected_model=model,selected_database=selection, payload=payload)
+                else:
+                    rag_db = load_rag_database(model=model, label=selection)
 
-        st.markdown("---")
+            # Create nested dictionary structure to allow different embeddings for the same documents - will be used for benchmarking
+            st.session_state.rag_databases.setdefault(selection, {})
+            st.session_state.rag_databases[selection][model] = rag_db
+
+        with st.expander("RAG Databases in Memory", expanded=True):
+
+            for label, models_dict in st.session_state.rag_databases.items():
+                with st.expander(f"**Label**:{label}", expanded=False):
+                    for model, rag_db in models_dict.items():
+                        with st.expander(f"**Model**:{model}", expanded=False), st.expander("Inspect Database", expanded=False):
+                            st.dataframe(rag_db.vector_db.database)
+                            if st.button("Store Database", key=f"store_rag_db_{label}_{model}"):
+                                parquet_embeddings = f"{DIRECTORY_EMBEDDINGS}/{selection}.parquet"
+                                rag_db.vector_db.database.write_parquet(parquet_embeddings) # noqa
+                                st.success(f"Stored RAG Database '{label}' to {parquet_embeddings}")
 
 @st.cache_resource
-def load_rag_database(doc_path: str, model: str, label: str, embedding_dimensions: Optional[int]=None) -> RagDatabase:
+def load_rag_database(model: str, label: str, embedding_dimensions: Optional[int]=None) -> RagDatabase:
     """
     Initialize RAG Database with .md documents.
     Loads existing embeddings if available.
@@ -66,61 +99,61 @@ def load_rag_database(doc_path: str, model: str, label: str, embedding_dimension
         rag_dataframe = pl.read_parquet(parquet_embeddings)
         rag_db = RagDatabase(model=model, database=rag_dataframe)
     else:
-        # Initialize empty RAG database
-        rag_db = RagDatabase(model=model, embedding_dimensions=embedding_dimensions)
+        raise FileNotFoundError(f"RAG database parquet file not found at {parquet_embeddings}. Please create the RAG database first.")
 
-    titles = []
-    texts = []
-    documents = [f for f in os.listdir(doc_path) if f.endswith('.md')]
-
-    for doc in documents:
-        with open(f"{doc_path}/{doc}", "r", encoding="utf-8") as f:
-            text = f.read()
-            if not rag_db.is_document_in_database(doc):
-                texts.append(text)
-                titles.append(doc)
-
-    if titles:
-
-        metadata_template = [{} for _ in range(len(titles))]
-        payload = RAGIngestionPayload.from_lists(titles=titles, texts=texts, metadata=metadata_template)
-        kwargs = {}
-        if "gemini" in model or "gemma" in model:
-            kwargs["task_type"] = "RETRIEVAL_DOCUMENT"
-
-        rag_db.add_documents(payload=payload, **kwargs)
     return rag_db
 
-def rag_workspace_obsidian() -> None:
-    """RAG Workspace for retrieval-augmented generation on specifiable obsidian vault."""
+@st.cache_data
+def generate_rag_database(
+    selected_model: str,
+    selected_database: str,
+    payload_path: Optional[str]=None,
+    payload: Optional[RAGIngestionPayload]=None
+    ) -> RagDatabase:
+    """Generate a RAG database from a payload parquet file or RAGIngestionPayload."""
 
-    rag_sidebar()
+    if payload_path is None and payload is None:
+        raise ValueError("Either payload_path or payload must be provided.")
 
+    if payload is None and payload_path is not None:
+        payload = RAGIngestionPayload.from_parquet(payload_path)
+
+    embedding_dimensions = MODEL_CONFIG[selected_model]["dimensions"]
+
+    embedding_path = Path(f"{DIRECTORY_EMBEDDINGS}/{selected_database}_{selected_model}_embeddings.parquet")
+    if embedding_path.exists():
+        rag_db = RagDatabase.from_parquet(embedding_path)
+    else:
+        embedding_dimensions = MODEL_CONFIG[selected_model]["dimensions"]
+        rag_db = RagDatabase(model=selected_model, embedding_dimensions=embedding_dimensions)
+
+    rag_db.add_documents(
+        payload=payload,
+        task_type="RETRIEVAL_DOCUMENT",
+    )
+
+    return rag_db
+
+def rag_workspace() -> None:
+    """RAG Workspace main function."""
+    st.title("RAG Workspace")
     with st._bottom:
-        rag_query = st.chat_input("Send a message", key="rag_input")
+        prompt = st.chat_input("Send a message", key="chat_input")
 
-    if rag_query:
-
-        rag_database = st.session_state.rag_databases[DATABASE_LABAL_OBSIDIAN][st.session_state.selected_embedding_model]
-        rag_query = RAGQuery(query=rag_query, k_documents=RAG_K_DOCS)
-
-        model = st.session_state.selected_embedding_model
-        kwargs = {}
-        if "gemini" in model or "gemma" in model:
-            kwargs["task_type"] = "RETRIEVAL_QUERY"
-
-        rag_response = rag_database.rag_process_query(rag_query, **kwargs)
-
+    if prompt:
+        rag_db: RagDatabase = st.session_state.rag_databases[st.session_state.selected_rag_database][st.session_state.selected_embedding_model] # noqa
+        query = RAGQuery(query=prompt, k_documents=5)
+        rag_response = rag_db.rag_process_query(rag_query=query)
         with st.chat_message("user"):
-            st.markdown(rag_query.query)
-
+            st.markdown(prompt)
         with st.chat_message("assistant"):
             documents = rag_response.to_polars()
             for doc in documents.iter_rows(named=True):
                 with st.expander(f"**Similarity**: {doc[DatabaseKeys.KEY_SIMILARITIES]:.2f}   -  **Title**: {doc[DatabaseKeys.KEY_TITLE]}"): # noqa
-                    st.markdown(doc[DatabaseKeys.KEY_TXT])
+                    st.markdown(doc[DatabaseKeys.KEY_TXT_RETRIEVAL])
 
 if __name__ == "__main__":
     st.set_page_config(page_title="RAG Workspace", page_icon=":robot:", layout="wide")
     init_rag_workspace()
-    rag_workspace_obsidian()
+    rag_sidebar()
+    rag_workspace()
